27c7429699fe1854af4ddd6f12424f55c48f9e7c
hannahk <hanhannah839@gmail.com>
2022-10-30 11:57:43 -0700

    fixed time color code

--- Develop/index.js
+++ Develop/index.js
@@ -1,60 +1,93 @@
-var container = document.querySelector(".container");
-var blocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var date = new Date();
+var $currentDay = $("#currentDay");
+var $timeBlocks = $(".time-block");
+var $writingarea = $(".schedule");
+var todos = [];
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H");   
 
-function isCurrentPastorFuture(hour) {
-  if (date.getHours() === hour) {
-    return 1; //present
-  } else if (date.getHours() > hour) {
-    return 2; //past
-  } else {
-    return 3; //future
-  }
+
+
+
+//looping through timeblocks.
+function initializeSchedule() {
+    
+    $timeBlocks.each(function () {
+        var $thisBlock = $(this);
+        var blockHour = parseInt($thisBlock.attr("current-hour"));
+
+        var list = {
+            hour: blockHour,
+            text: "",
+        }
+
+    todos.push(list);
+    }); 
+
+  localStorage.setItem("listOfThings", JSON.stringify(todos));
+  console.log(todos);
 }
 
 
-for (var i = 0; i < blocks.length; i++) {
-  var wrapper = document.createElement("div");
-  wrapper.setAttribute("class", "row time-block");
-  if (isCurrentPastorFuture(blocks[i]) === 1) {
-    wrapper.classList.add("present");
-  } else if (isCurrentPastorFuture(blocks[i]) === 2) {
-    wrapper.classList.add("past");
-  } else {
-    wrapper.classList.add("future");
-  }
+// colr code past/present/future based on the hour
+function colorCodeTimeBlocks() {
+   
+    $timeBlocks.each(function() {
+        var $thisBlock = $(this);
+        var blockHour = parseInt($thisBlock.attr("current-hour"));
 
-  var url = "https://cdn-icons-png.flaticon.com/512/2874/2874091.png";
-    $(document).ready(function() {
-        var image = new Image();
-        image.src = url;
-        image.style.width = '20px';
-        image.style.height = 'auto';
-        image.style.position = 'absolute';
-    
-        $(".saveBtn").append(image);   
-
-        $(".saveBtn").mouseover(function() {
-            $(this).css("background-color","#154c79")
-          }).mouseout(function() {
-            $(this).css("background-color","#06aed5")
-          });
-
-        $(".saveBtn").on("click", function () {
-            var value = $(this).siblings(".description").val();
-            var time = $(this).siblings(".hour").val();
-        
-            
-            localStorage.setItem(time, value);
-        
-            $(".notification").addClass("show");
-            
-            setTimeout(function () {
-              $(".notification").removeClass("show");
-            }, 5000);
-
-            localStorage.getItem(time, value);
-                console.log(localStorage);
-          });
+        if (blockHour == currentHour) {
+            $thisBlock.addClass("present").removeClass("past future");
+        }
+        else if (blockHour < currentHour) {
+            $thisBlock.addClass("past").removeClass("present future");
+        } 
+        else {
+            $thisBlock.addClass("future").removeClass("present past");
+        }  
     });
 }
+
+function renderLocalStorage() {
+   
+    for (var i = 0; i < todos.length; i++) {
+        var everyHour = todos[i].hour;
+        var everyText = todos[i].text;
+
+        $("[current-hour=" + everyHour + "]").children("textarea").val(everyText);
+    };
+    console.log(todos);
+    
+}
+
+function init() {
+    var storedTodos = JSON.parse(localStorage.getItem("listofThings"));
+    
+    if (storedTodos !== null) {
+        document.getElementById("save-hour") = storedTodos;
+      }
+
+    renderLocalStorage();
+}
+
+function storeTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+$(document).ready(function () {
+    colorCodeTimeBlocks();
+    
+    $('#currentDay').text(currentDate);
+ 
+
+    $("button").on("click", function () {
+            
+            $(".notification").addClass( "show"),
+
+        setTimeout(function () {
+            $(".notification").removeClass("show");
+        }, 5000);
+    });
+
+});
+
+init()
