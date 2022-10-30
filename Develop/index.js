d63d40a45e2cbff406ba90104f0f7879a754a108
hannahk <hanhannah839@gmail.com>
2022-10-29 23:16:45 -0700

    added html and more javacsript

--- Develop/index.js
+++ Develop/index.js
@@ -1,51 +1,60 @@
 var container = document.querySelector(".container");
-var blocks = [9,10,11,12,13,14,15,16,17]
+var blocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var date = new Date();
 
+function isCurrentPastorFuture(hour) {
+  if (date.getHours() === hour) {
+    return 1; //present
+  } else if (date.getHours() > hour) {
+    return 2; //past
+  } else {
+    return 3; //future
+  }
+}
 
 
- function isCurrentPastorFuture(hour) {
-    if (date.getHours() === hour) {
-        return 1 //present
-    } else if (date.getHours() > hour) {
-        return 2 //past
-    } else {
-        return 3 //future
-    }
- }
+for (var i = 0; i < blocks.length; i++) {
+  var wrapper = document.createElement("div");
+  wrapper.setAttribute("class", "row time-block");
+  if (isCurrentPastorFuture(blocks[i]) === 1) {
+    wrapper.classList.add("present");
+  } else if (isCurrentPastorFuture(blocks[i]) === 2) {
+    wrapper.classList.add("past");
+  } else {
+    wrapper.classList.add("future");
+  }
 
- for (var i = 0; i < blocks.length; i++) {
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "row timeblock");
-    if (isCurrentPastorFuture(blocks[i]) === 1) {
-        wrapper.classList.add("present");
-    } else if (isCurrentPastorFuture(blocks[i]) === 2) {
-        wrapper.classList.add("past");
-    } else {
-        wrapper.classList.add("future");
-    }
-    var block = document.createElement("div");
-    var textbox = document.createElement("textarea");
-    var savebutton = document.createElement("finalsave");
-    savebutton.setAttribute("class", ".saveBtn, btn");
-        $('.saveBtn').on('click', function () {
-            var value = $(this).siblings('.description').val();
-            var time = $(this).parent().attr('id');
+  var url = "https://cdn-icons-png.flaticon.com/512/2874/2874091.png";
+    $(document).ready(function() {
+        var image = new Image();
+        image.src = url;
+        image.style.width = '20px';
+        image.style.height = 'auto';
+        image.style.position = 'absolute';
+    
+        $(".saveBtn").append(image);   
 
-            localStorage.setItem(time,value);
+        $(".saveBtn").mouseover(function() {
+            $(this).css("background-color","#154c79")
+          }).mouseout(function() {
+            $(this).css("background-color","#06aed5")
+          });
 
-            $('.notification').addClass('show');
+        $(".saveBtn").on("click", function () {
+            var value = $(this).siblings(".description").val();
+            var time = $(this).siblings(".hour").val();
+        
+            
+            localStorage.setItem(time, value);
+        
+            $(".notification").addClass("show");
+            
             setTimeout(function () {
-                $('.notification').removeClass('show');
+              $(".notification").removeClass("show");
             }, 5000);
-        });
-    textbox.setAttribute("class", "description col-md-10");
-    block.setAttribute("class", "hour");
-    textbox.setAttribute("style", "flex-grow: 1; height: 100px;");
-    block.textContent = moment(blocks[i], 'HH').format('h a');
-    wrapper.appendChild(block);
-    wrapper.appendChild(textbox);
-    container.appendChild(wrapper);   
-    wrapper.appendChild(savebutton);
-    savebutton.textContent= "save"
- }
+
+            localStorage.getItem(time, value);
+                console.log(localStorage);
+          });
+    });
+}
