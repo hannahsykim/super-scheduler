7b21fcb3709c08134a5c7a4b3c5f247153dd1b6a
hannahk <hanhannah839@gmail.com>
2022-11-01 18:29:09 -0700

    final edit, fully functional

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,28 @@
 //When the page loads
 $(document).ready(function () {
+  console.log("ready!");
   var $timeBlocks = $(".time-block");
   var currentDate = moment().format("dddd, MMMM Do");
   var currentHour = moment().format("H");
-  var todos = [];
 
-  function saveSchedule() {
-    todos = [];
-    //for every block
-    $timeBlocks.each(function () {
-      var $thisBlock = $(this);
-      var blockHour = parseInt($thisBlock.attr("current-hour"));
-      //console.log($thisBlock);
-      var textArea = $thisBlock.children("textarea");
-      //console.log(textArea);
-      var list = {
-        hour: blockHour,
-        text: textArea.val(),
-      };
-      todos.push(list);
-    });
+  //when the schedule is saved, show notification and remove it
+  $(".saveBtn").on("click", function () {
+    //save local storage for every block after each btn click
 
-    console.log(todos);
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-    //set item to localstorage
-    localStorage.setItem("listOfThings", JSON.stringify(todos));
-  }
+    localStorage.setItem(time, value);
+
+    // this shows the notification and removes it after 5 seconds
+    $(".notification").addClass("show"),
+      setTimeout(function () {
+        $(".notification").removeClass("show");
+      }, 5000);
+  });
+
+  //display current date
+  $("#currentDay").text(currentDate);
 
   // color code past/present/future based on the hour
   function colorCodeTimeBlocks() {
@@ -42,43 +39,19 @@ $(document).ready(function () {
       }
     });
   }
-
-  function renderSchedule() {
-    var toDoItems = JSON.parse(localStorage.getItem("listOfThings"));
-
-    //loop thru array then assign the text to the timeBlock with data-hour equal to hour.
-    for (var i = 0; i < toDoItems.length; i++) {
-      var itemHour = toDoItems[i].hour;
-      var itemText = toDoItems[i].text;
-      //plug it into a variable
-      $(itemHour).val(itemText);
-    }
-    console.log(toDoItems);
-  }
-
-  // TODO - recover all todos from localStorage on page is loaded, and show in UI 
-  renderSchedule();
-
   //show timeblocks depending on time
   colorCodeTimeBlocks();
-
   var interval = setInterval(colorCodeTimeBlocks, 15000);
 
-  //display current date
-  $("#currentDay").text(currentDate);
-  
-  //when the schedule is saved, show notification and remove it
-  $(".saveBtn").on("click", function () {
-    //save local storage for every block after each btn click
-    saveSchedule();
-    
-    // this shows the notification and removes it after 5 seconds
-    $(".notification").addClass("show"),
-      setTimeout(function () {
-        $(".notification").removeClass("show");
-      }, 5000);
-  });
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-  
-
+  console.log(localStorage);
 });
